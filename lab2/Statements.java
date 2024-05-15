572cadc594f492cb2200d2737ceb86935eff5033
deekshithapendyala <deekshitha11020@gmail.com>
2024-05-15 14:02:12 +0530

    Add files via upload

---
+++ lab2/Statements.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class Statements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String login="deekshitha";
+		if(login=="deekshitha") {
+			System.out.println("user is valid");
+		}
+		else 
+		{
+			System.out.println("user is not valid");
+		}
+		
+	}
+
+}
